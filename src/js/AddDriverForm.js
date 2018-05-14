3ee965bfb230886a25c07f23b9855c76eb8d29cc
Elena Israfilova <malvinkooo@gmail.com>
2018-05-14 17:56:27 +0300

    Add Driver form validation #18

--- src/js/AddDriverForm.js
+++ src/js/AddDriverForm.js
@@ -5,6 +5,42 @@ class AddDriverForm{
         this._carsController = null;
         this._selectDropDown = this._addDriverFormElement.find("select.cars-list");
         this._addDriverFormElement.find(".submit").click(this._onAddDriverFormSubmit.bind(this));
+        this._addDriverFormConstraints = {
+            name: {
+                format: {
+                    pattern: /[А-Яа-я-ёЁ]+/,
+                    message: "^Пожалуйста, заполните это поле. Имя может состоять только из букв."
+                },
+                length: {
+                    minimum: 2,
+                    maximum: 30,
+                    message: "^Имя должно состоять максимум из 30 символов и минимум из 2."
+                }
+            },
+            surname: {
+                format: {
+                    pattern: /[А-Яа-я-ёЁ]+/,
+                    message: "^Пожалуйста, заполните это поле. Фамилия может состоять только из букв."
+                },
+                length: {
+                    minimum: 2,
+                    maximum: 30,
+                    message: "^Фамилия должна состоять максимум из 30 символов и минимум из 2."
+                }
+            },
+            phone: {
+                format: {
+                    pattern: /^\+?(\d+(\#\d+)?){1,30}/,
+                    message: "^Это поле обязательно для заполнения. Номер телефона не может превышать 30 символов и должен состоять только из цифр."
+                }
+            },
+            description: {
+                length: {
+                    maximum: 255,
+                    message: "^Количество символов не должно быть больше 255."
+                }
+            }
+        };
     }
 
     setDriversController(driversController) {
@@ -33,7 +69,22 @@ class AddDriverForm{
             var element = $(elements[i]);
             driverParams[element.attr('name')] = element.val();
         }
-        this._driversController.addDriver(driverParams);
-        this._addDriverFormElement.find("form")[0].reset();
+        var errors = validate(driverParams, this._addDriverFormConstraints);
+        for(var k = 0; k < elements.length; k++) {
+            var field = $(elements[k]).closest(".field");
+            console.log(field);
+            field.removeClass("has-error");
+            field.find(".help-error").remove();
+            if(errors) {
+                var errorsInput = errors[$(elements[k]).attr("name")];
+                if(errorsInput) {
+                    field.addClass("has-error").append("<p class='help-error'>"+errorsInput+"<p>");
+                }
+            }
+        }
+        if(!errors) {
+            this._driversController.addDriver(driverParams);
+            this._addDriverFormElement.find("form")[0].reset();
+        }
     }
 }
