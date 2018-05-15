1525e2edf0f8ce5ad32daaeda6c33b62f9a33891
Elena Israfilova <malvinkooo@gmail.com>
2018-05-15 12:27:16 +0300

    Add order form validation

--- src/js/AddOrderForm.js
+++ src/js/AddOrderForm.js
@@ -10,6 +10,63 @@ class AddOrderForm {
         this._destinationSearchBox = new SearchBox(this._addOrderFormElement.find(".search.destination"));
         this._isSetDriverAutomaticallyChecked = false;
         this._addOrderFormElement.find('.submit').click(this._onAddOrderFormSubmit.bind(this));
+        this._addOrderFormConstraints = {
+            clientName: {
+                presence: {
+                    allowEmpty: false,
+                    message: "^Пожалуйста, заполните это поле."
+                },
+                format: {
+                    pattern: /[А-Яа-я-ёЁ]*/,
+                    message: "^Имя может состоять только из букв."
+                },
+                length: {
+                    minimum: 2,
+                    maximum: 30,
+                    message: "^Имя должно состоять максимум из 30 символов и минимум из 2."
+                }
+            },
+            clientPhone: {
+                presence: {
+                    allowEmpty: false,
+                    message: "^Это поле обязательно для заполнения."
+                },
+                format: {
+                    pattern: /^\+?(\d+(\#\d+)?){1,30}/,
+                    message: "^Номер телефона не может превышать 30 символов и должен состоять только из цифр."
+                }
+            },
+            carFeedPoint: {
+                presence: {
+                    allowEmpty: false,
+                    message: "^Пожалуйста, заполните это поле."
+                },
+                length: {
+                    maximum: 50,
+                    message: "^Адрес может состоять максимум из 50 символов"
+                }
+            },
+            destination: {
+                presence: {
+                    allowEmpty: false,
+                    message: "^Пожалуйста, заполните это поле."
+                },
+                length: {
+                    maximum: 50,
+                    message: "^Адрес может состоять максимум из 50 символов"
+                }
+            },
+            rate: {
+                presence: {
+                    allowEmpty: false,
+                    message: "^Пожалуйста, заполните поле."
+                },
+                numericality: {
+                    greaterThanOrEqualTo: 0,
+                    message: "^Допустимы только положительные числа."
+                }
+            }
+        };
     }
 
     setOrdersController(ordersController) {
@@ -27,9 +84,22 @@ class AddOrderForm {
             orderParams[$(elements[i]).attr('name')] = $(elements[i]).val();
         }
         orderParams.isSetDriverAutomatically = this._isSetDriverAutomaticallyChecked;
-        this._ordersController.addOrder(orderParams);
-        this._addOrderFormElement.find('form')[0].reset();
-        return false;
+        var errors = validate(orderParams, this._addOrderFormConstraints);
+        for(var k = 0; k < elements.length; k++) {
+            var field = $(elements[k]).closest(".field");
+            field.removeClass("has-error");
+            field.find(".help-error").remove();
+            if(errors) {
+                var errorsInput = errors[$(elements[k]).attr("name")];
+                if(errorsInput) {
+                    field.addClass("has-error").append("<p class='help-error'>"+errorsInput+"</p>");
+                }
+            }
+        }
+        if(!errors) {
+            this._ordersController.addOrder(orderParams);
+            this._addOrderFormElement.find('form')[0].reset();
+        }
     }
 
     show() {
